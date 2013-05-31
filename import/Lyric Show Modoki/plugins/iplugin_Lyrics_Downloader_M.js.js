0fe839fcb6dbe220105fad5c893df829ad07cccf
tomato111 <ashiatoxx@gmail.com>
2013-05-31 12:10:50 +0900

    delete iplugin

--- import/Lyric Show Modoki/plugins/iplugin_Lyrics_Downloader_M.js.js
+++
@@ -1,9 +0,0 @@
-﻿iplugin_Lyrics_Downloader_M = {
-    name: "iplugin_Lyrics_Downloader_M",
-    commandName: 'Instructions_Lyrics_Downloader_M',
-    label: prop.Panel.Lang == 'ja' ? '検索指示: Lyrics Downloader (Miku Hatsune wiki)' : 'Instructions: Lyrics Downloader (Miku Hatsune wiki)',
-    author: 'Tomato',
-    onCommand: function () {
-        window.NotifyOthers("Lyrics Downloader (Miku Hatsune wiki)", "");
-    }
-};
